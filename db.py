33e3fd4fc56a2778f60b52ed68f0c873f79b6650
Michael Smith <michael.smith@gmail.com>
2025-12-30 10:32:35 -0800

    Switch to SQLAlchemy + pytds for Azure SQL

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
 import os
-import pymssql
+from sqlalchemy import create_engine
 
 def get_connection():
     connection_string = os.getenv("CONNECTION_STRING")
 
-    # Parse the connection string into a dictionary
+    # Convert your Azure SQL connection string into a SQLAlchemy URL
     parts = dict(
         item.split('=', 1)
         for item in connection_string.split(';')
@@ -12,13 +12,12 @@ def get_connection():
     )
 
     server = parts['Server'].replace('tcp:', '').split(',')[0]
+    database = parts['Database']
     user = parts['Uid']
     password = parts['Pwd']
-    database = parts['Database']
 
-    return pymssql.connect(
-        server=server,
-        user=user,
-        password=password,
-        database=database
-    )
+    # SQLAlchemy URL using pytds
+    url = f"mssql+pytds://{user}:{password}@{server}/{database}"
+
+    engine = create_engine(url)
+    return engine.connect()
