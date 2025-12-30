28e0db45715ccf14b8db4f0dd90778413e0e13d5
Michael Smith <michael.smith@gmail.com>
2025-12-30 13:47:17 -0800

    Test out Azure Link

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def home():
 def testdb():
     try:
         conn = get_connection()
-        return "Connected to SQL!"
+        return "Connected to SQL. Wooo!"
     except Exception as e:
         return str(e), 500
 
