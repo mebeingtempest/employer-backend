bacc56e5a83c10b4a6bc773ee6c7b9e4db55856a
Michael Smith <michael.smith@gmail.com>
2025-12-30 10:20:51 -0800

    Switch from pyodbc to pymssql

--- db.py
+++ db.py
@@ -1,6 +1,24 @@
 import os
-import pyodbc
+import pymssql
 
 def get_connection():
     connection_string = os.getenv("CONNECTION_STRING")
-    return pyodbc.connect(connection_string)
+
+    # Parse the connection string into a dictionary
+    parts = dict(
+        item.split('=', 1)
+        for item in connection_string.split(';')
+        if '=' in item
+    )
+
+    server = parts['Server'].replace('tcp:', '').split(',')[0]
+    user = parts['Uid']
+    password = parts['Pwd']
+    database = parts['Database']
+
+    return pymssql.connect(
+        server=server,
+        user=user,
+        password=password,
+        database=database
+    )
